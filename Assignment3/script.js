f3f249d0fa77e7c297d4dc2558aea13f3fff0ca9
RoopaAenugu <151902380+RoopaAenugu@users.noreply.github.com>
2024-08-02 23:51:46 +0530

    Add files via upload

---
+++ Assignment3/script.js
@@ -0,0 +1,94 @@
+let current = '';
+let memory;
+let d = document.getElementById("displaytext");
+
+function memoryclear() {
+    memory = '';
+}
+
+function memoryRead() {
+    d.value = memory;
+}
+
+function memoryStorage() {
+    memory = Number(d.value);
+}
+
+function memoryAppend() {
+    memory = memory + Number(d.value);
+    d.value = memory;
+}
+
+function memorySub() {
+    memory = memory - Number(d.value);
+    d.value = memory;
+}
+
+function clearDisplay() {
+    current = '';
+    d.value = '';
+}
+
+function addSubSign() {
+    if (d.value !== '') {
+        d.value = (-1 * Number(d.value)).toString();
+        current = d.value;
+    }
+}
+
+function squareRoot() {
+    if (d.value !== '') {
+        d.value = Math.sqrt(Number(d.value)).toString();
+        current = d.value;
+    }
+}
+
+function append(buttonValue) {
+    if (!isNaN(buttonValue) || buttonValue === '.') {
+        // If the button value is a number or decimal point, append it to the current value
+        current += buttonValue;
+    } else {
+        // Handle operators
+        switch (buttonValue) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                // If current ends with an operator, replace it
+                if (['+', '-', '*', '/'].includes(current.slice(-1))) {
+                    current = current.slice(0, -1);
+                }
+                current += buttonValue;
+                break;
+            case '=':
+                calculateResult();
+                break;
+             case '%':
+                    k=Number(current);
+                    k=k/100;
+                    current=k.toString();
+                    break;
+               
+             case '1/x':
+                   
+                    k=Number(current);
+                   console.log(k);
+                   current=(1/k).toString();
+                  break;
+        }
+    }
+    // Update the display with the current value
+    d.value = current;
+}
+
+function calculateResult() {
+    console.log(current);
+    try {
+        // Evaluate the current expression
+        current = eval(current).toString();
+    } catch (e) {
+        current = 'Error';
+    }
+    d.value = current;
+    current = d.value;
+}
