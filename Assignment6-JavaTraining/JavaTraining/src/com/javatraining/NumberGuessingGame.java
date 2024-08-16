447e3941dfe167ecef615cc5b42854b51badda80
RoopaAenugu <151902380+RoopaAenugu@users.noreply.github.com>
2024-08-16 11:53:56 +0530

    Update NumberGuessingGame.java

--- Assignment6-JavaTraining/JavaTraining/src/com/javatraining/NumberGuessingGame.java
+++ Assignment6-JavaTraining/JavaTraining/src/com/javatraining/NumberGuessingGame.java
@@ -4,26 +4,19 @@ import java.util.Scanner;
 
 public class NumberGuessingGame {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int c = (int) Math.ceil(Math.random() * 100);
+        Scanner scanner = new Scanner(System.in);
+        int randomNumber = (int) Math.ceil(Math.random() * 100);
         while (true) {
             System.out.println("enter value");
-            int k=sc.nextInt();
-            if(k<c){
+            int userInput = scanner.nextInt();
+            if (userInput < randomNumber) {
                 System.out.println("enter value is too low");
-            }
-            else if(k>c){
+            } else if (userInput > randomNumber) {
                 System.out.println("enter value is too high");
-            }
-            else{
+            } else {
                 System.out.println("congratulations your value is correct");
                 break;
             }
-
-
         }
     }
-
-
-
 }
